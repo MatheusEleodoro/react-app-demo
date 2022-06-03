12a0300544853b106a102e3350a9c07c9e7777fc
Matheus Santos Eleodoro de Souza <matheusses@triadsystems.com.br>
2022-06-03 17:00:47 -0300

    :sparkles: create(Home)

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,57 +1,18 @@
-import React, { useState } from 'react';
-import icon from './assets/img/icon.png';
-import './styles.css';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
-  const[email,setEmail] = useState('')
-  const[senha,setSenha] = useState('');
-
-
   return (
-    <div className="container">
-      <div className="container-login">
-        <div className="wrap-login">
-          <form className="login-form">
-            
-          <span className="login-form-title">
-              <img src={icon} alt="icon" />
-            </span>
-
-            <span className="login-form-title">Eae meu chapa!</span>
-            <span className="login-form-title">vamos logar?</span>
-            
-            <div className='wrap-input'>
-              <input 
-               className={email !== ""? 'has-value input':'input'} 
-               type='email'
-               value={email}
-               onChange={e=>setEmail(e.target.value)}/>
-              <span className="focus-input" data-placeholder='Email'/>
-            </div>
-
-            <div className='wrap-input'>
-              <input 
-              className={senha !== ""? 'has-value input':'input'} 
-              type='password'
-              value={senha}
-              onChange={e=>setSenha(e.target.value)}/>
-              <span className="focus-input" data-placeholder='Senha'/>
-            </div>
-
-            <div className="container-login-form-btn">
-              <button className="login-form-btn">Login</button>
-            </div>
-
-            <div className="text-center">
-              <span className="txt1">Não possui acesso?</span>
-              <a href="#" className="txt2">Criar conta</a>
-            </div>
-
-          </form>
-        </div>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
